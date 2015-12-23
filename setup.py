e29be78b3ba4e847e19ed1d8b941ecdf49689e26
Reuben Cummings <reubano@gmail.com>
2015-12-23 20:05:46 +0300

    Add PyPy to setup.py

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Intended Audience :: End Users/Desktop',
