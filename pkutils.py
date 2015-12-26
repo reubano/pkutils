27c6c4af45bfa1e1d9584ff2e3d4824a67176abf
Reuben Cummings <reubano@gmail.com>
2015-12-26 23:04:09 +0300

    Bump to version 0.9.0

--- pkutils.py
+++ pkutils.py
@@ -28,7 +28,7 @@ from functools import total_ordering
 from bisect import bisect
 from builtins import *
 
-__version__ = '0.8.0'
+__version__ = '0.9.0'
 
 __title__ = 'pkutils'
 __author__ = 'Reuben Cummings'
