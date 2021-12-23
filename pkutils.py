d7e6acd4f56dcf24f2cf4d687a4506c64625e3fa
Reuben Cummings <reubano@gmail.com>
2021-12-22 19:11:30 -0600

    Bump to version 3.0.0

--- pkutils.py
+++ pkutils.py
@@ -31,7 +31,7 @@ from functools import total_ordering
 
 import semver
 
-__version__ = "2.1.1"
+__version__ = "3.0.0"
 
 __author__ = "Reuben Cummings"
 __description__ = "Python packaging utility library"
